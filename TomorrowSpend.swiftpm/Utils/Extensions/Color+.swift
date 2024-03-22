cad04f5357ddebec67c8d3f008d2489624b95aab
Nam Yuseong <75793880+99yuseong@users.noreply.github.com>
2024-03-22 09:35:15 +0900

    Add Color Extension

---
+++ TomorrowSpend.swiftpm/Utils/Extensions/Color+.swift
@@ -0,0 +1,44 @@
+//
+//  Color+.swift
+//
+//
+//  Created by 남유성 on 3/22/24.
+//
+
+import SwiftUI
+
+extension Color {
+    static let main = Color(hex: "#35F275")
+    static let mainBg = Color(hex: "#35D96D")
+    static let accent = Color(hex: "#F27535")
+    static let appPrimary = Color(hex: "#1C1C1C")
+}
+
+extension Color {
+    init(hex: String) {
+        let scanner = Scanner(string: hex)
+        _ = scanner.scanString("#")
+        
+        var rgb: UInt64 = 0
+        scanner.scanHexInt64(&rgb)
+        
+        let r = Double((rgb >> 16) & 0xFF) / 255.0
+        let g = Double((rgb >>  8) & 0xFF) / 255.0
+        let b = Double((rgb >>  0) & 0xFF) / 255.0
+        
+        self.init(red: r, green: g, blue: b)
+    }
+}
+
+#Preview {
+    VStack {
+        Rectangle()
+            .foregroundStyle(Color.main)
+        Rectangle()
+            .foregroundStyle(Color.mainBg)
+        Rectangle()
+            .foregroundStyle(Color.accent)
+        Rectangle()
+            .foregroundStyle(Color.primary)
+    }
+}
